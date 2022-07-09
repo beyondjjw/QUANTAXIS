0e2924ddbc1f61c4a5a38295919d96e3f288a720
beyondjjw <148323745@qq.com>
2022-07-09 22:59:50 +0800

    add create volume shell script
    Signed-off-by: beyondjjw <148323745@qq.com>

---
+++ docker/qa-service-pro/createvolume.sh
@@ -0,0 +1,7 @@
+docker volume create qamg
+docker volume create qacode
+docker volume create pg-data
+docker volume create qadag
+docker volume create qaconf
+docker volume create qauser
+docker volume create qalog

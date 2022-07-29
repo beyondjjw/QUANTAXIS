d17ce5f1678b44394a0620e145b92e012b8e9257
beyondjjw <148323745@qq.com>
2022-07-30 06:38:45 +0800

    modify
    Signed-off-by: beyondjjw <148323745@qq.com>

--- backtraderex/demo1.py
+++ backtraderex/demo1.py
@@ -11,32 +11,35 @@ from QUANTAXIS.QAUtil.QAParameter import (DATABASE_TABLE, DATASOURCE,
                                           FREQUENCE, MARKET_TYPE,
                                           OUTPUT_FORMAT)
 
+
 def get_yahoo_csv(code='000001'):
     data_path = Path(os.getcwd()) / 'datas/000001.SZ.csv'
     date_frame = pd.read_csv(data_path)
     date_frame['datetime'] = pd.to_datetime(date_frame['Date'])  #
     date_frame.set_index('datetime', inplace=True)  #
     date_frame['openinterest'] = 0
-    
+
     return date_frame
 
+
 def get_mongo_data(code='000001'):
-    df = QA_quotation_adv('000001', '2019-12-01', '2022-06-01', frequence=FREQUENCE.DAY,
+    df = QA_quotation_adv('000001', '2022-05-01', '2022-06-01', frequence=FREQUENCE.ONE_MIN,
                           market=MARKET_TYPE.STOCK_CN, source=DATASOURCE.AUTO, output=OUTPUT_FORMAT.DATAFRAME)
     df.reset_index(drop=False, inplace=True)
-    df.drop(['code'],axis=1, inplace=True)
-    df['datetime'] = pd.to_datetime(df['date'])  #
+    # df.drop(['code'], axis=1, inplace=True)
+    # df['datetime'] = pd.to_datetime(df['date'])  #分钟线只有datetime, 日线只有date
     df.set_index('datetime', inplace=True)  #
     df['openinterest'] = 0
-    
-    return df 
+
+    return df
 
 
 def write_csv_to_compare_datas(first, second):
     first.to_csv('000001.first.day.csv', index=True)
     second.to_csv('000001.second.day.csv', index=True)
-    
 
+
+# 可以先写一个 dual thrust 策略测试一下
 class TestStrategy(bt.Strategy):
     params = (
         ('maperiod', 15),
@@ -45,6 +48,8 @@ class TestStrategy(bt.Strategy):
     def __init__(self):
         # 将第一个data给变量dataclose, close 是Lines对象
         self.dataclose = self.datas[0].close
+        # 主数据源是分钟数据， 日线数据是投射分钟线加入到cerebro, 所以日线数据再画图的时候就不显示了
+        self.data1.plotinfo.plot = False
         print("initialization")
 
     def start(self, order=None):
@@ -61,7 +66,7 @@ class TestStrategy(bt.Strategy):
         self.log("Close {}".format(self.data.close[0]))
 
     def log(self, txt):
-        dt = self.datas[0].datetime.date(0)
+        dt = self.datas[0].datetime.datetime(0)
         print("{} {}".format(dt.isoformat(), txt))  #
 
     def stop(self):
@@ -72,7 +77,6 @@ class TestStrategy(bt.Strategy):
         print("notify_order strategy")
 
 
-
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(50000)
@@ -81,13 +85,17 @@ if __name__ == '__main__':
     yahoo = get_yahoo_csv()
     mongo = get_mongo_data()
     write_csv_to_compare_datas(yahoo, mongo)
-   
-    daily_data = bt.feeds.PandasData(dataname=mongo,
-                                     fromdate=datetime.datetime(2010, 5, 25),
-                                     todate=datetime.datetime(2022, 6, 1)
-                                     )
 
-    cerebro.adddata(daily_data)
+    min_bar = bt.feeds.PandasData(dataname=mongo,
+                                  fromdate=datetime.datetime(2010, 5, 25),
+                                  todate=datetime.datetime(2022, 6, 1),
+                                  timeframe=bt.TimeFrame.Minutes
+                                  )
+
+    cerebro.adddata(min_bar)  # 第一个被加进去的数据叫main_bar，可以用来交易
+    # resample先投射数据，再adddata到cerebro
+    # 分钟线加进去，可以按照交易日投射， 投射后的日线可以用来产生信号
+    cerebro.resampledata(min_bar, timeframe=bt.TimeFrame.Days)
 
     cerebro.addstrategy(TestStrategy)
     # cerebro.optstrategy(TestStrategy,
@@ -98,3 +106,4 @@ if __name__ == '__main__':
     print('End portfolio {}'.format(cerebro.broker.getcash()))
 
     # cerebro.plot(style='candle')  #
+    cerebro.plot()
